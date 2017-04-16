c4bf84f49b525739546896a4a3137c411ca32fa5
wuke1993 <2297184250@qq.com>
2017-04-16 13:31:22 +0800

    Add myLocks.TestLocks.

---
+++ src/myLocks/TestLocks.java
@@ -0,0 +1,21 @@
+package myLocks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author: wuke 
+ * @date  : 2017年4月12日 下午2:28:13
+ * Title  : TestLocks
+ * Description : 
+ */
+public class TestLocks {
+	
+	public static void main(String[] args) {
+		Lock lock;
+		ReentrantLock rLock;
+		ReadWriteLock rwLock;
+	}
+
+}
